80e251ddfa18c37c40fa379e328a02d2ffdbbb0a
Mateus <mateusoliveiraalmeida@gmail.com>
2021-08-14 01:02:15 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e20344b672ee8094b57cf07266cf6fb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e2569027-3f5c-49fa-89b6-ced9eee88abb'
+}; 
